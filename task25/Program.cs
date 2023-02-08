107904ecf1ec5f3f944ab36d002cf451573128e4
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-08 23:03:39 +0500

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую нужно возвести это число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int xn = 1;
+for(int i = 0; i < n; i++)
+{
+    xn = xn * x;
+}
+Console.WriteLine($"Число {x} в степени {n} равно {xn}");
