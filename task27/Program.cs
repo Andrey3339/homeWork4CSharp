87bea30719016bc10770e8736ebd22fe08bc1b01
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-08 23:20:34 +0500

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число, сумму цифр которого нужно узнать: ");
+string xString = Console.ReadLine();
+long sum = 0;
+long x = Convert.ToInt64(xString.Trim());
+long xPart = x;
+for(int i = 0; i < xString.Trim().Length; i++)
+{
+    sum = sum + (xPart % 10);
+    xPart = xPart / 10;
+}
+Console.WriteLine($"Сумма цифр числа {x} равна {sum}.");
+
